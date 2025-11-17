b8a8f8e197ef71bb44f0deb15271e89093368d42
Walax Azevedo <walaxazevedo728@gmail.com>
2025-11-16 22:03:59 -0300

    correção do manifest do NGINX com SSL para garantir o funcionamento do DNS com certificado válido; refatoração de outros manifests

--- health/health.check.sh
+++ health/health.check.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-echo "Health Check - Whoami"
-
 INGRESS_HOST="devops-walax.ddns.net"
 
+echo "Health Check - Whoami"
+echo "----------------------"
+
 echo "Testando HTTP..."
-curl -s -o /dev/null -w "%{http_code}" http://$INGRESS_HOST
+curl -I http://$INGRESS_HOST
 
+echo ""
 echo "Testando HTTPS..."
-curl -s -o /dev/null -w "%{http_code}" https://$INGRESS_HOST
+curl -I https://$INGRESS_HOST
 
-echo "Testando Readiness..."
-kubectl get pods -n devops-challenge -o json \
-  | jq '.[].status.conditions[] | select(.type=="Ready")'
+echo ""
+echo "Verificando Pods..."
+kubectl get pods -n devops-challenge
